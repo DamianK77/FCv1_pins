17e1bd9d82098a6f5b41ffc57ada5520d29f2d27
DamianK77 <damik422@gmail.com>
2024-04-12 10:06:43 +0200

    update pin nums

--- include/FCv1_pins.h
+++ include/FCv1_pins.h
@@ -1,1 +1,20 @@
+#define FC_PIN_M0 7
+#define FC_PIN_M1 15
+#define FC_PIN_M2 16
+#define FC_PIN_M3 17
+#define FC_PIN_M4 18
+#define FC_PIN_M5 8
+
+#define FC_PIN_RX1 41
+#define FC_PIN_TX1 42
+#define FC_PIN_RX2 39
+#define FC_PIN_TX2 40
+#define FC_PIN_SCL1 47
+#define FC_PIN_SDA1 48
+#define FC_PIN_SCL_IMU 9
+#define FC_PIN_SDA_IMU 3
+
+#define FC_PIN_LED1 1
+#define FC_PIN_LED2 2
+
 void func(void);
